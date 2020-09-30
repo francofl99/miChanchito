9ea1fc69fca2c584c4a22a8c521c0989f151d86a
Franco <francofl1999@gmail.com>
2020-09-30 11:09:21 -0300

    minimal changes

--- resources/js/components/SideBar.vue
+++ resources/js/components/SideBar.vue
@@ -3,13 +3,24 @@
     class="bg-pink-200 w-12 py-64 flex flex-wrap items-center justify-center"
     style="height: calc(100vh - 2.5rem)"
   >
-    <div class="w-10 h-10 bg-pink-300 hover:bg-pink-400 transition duration-150 rounded-md mx-auto cursor-pointer" />
+    <div class="w-10 h-10 bg-pink-300 hover:bg-pink-400 transition duration-150 rounded-md mx-auto cursor-pointer flex justify-center ">
+      <VueHicons
+        name="home"
+        width-icon="8"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import VueHicons from "vue-hicons";
+
 export default {
   name: "SideBar",
+
+  components: {
+    VueHicons
+  }
 
 };
 </script>
